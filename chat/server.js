ce9c9652c80f453a99f184c324750dc11b559246
Gauthier Cler <gauthier.cler@epitech.eu>
2017-04-14 20:16:44 +0200

    fixed chat hostname and port

--- chat/server.js
+++ chat/server.js
@@ -5,8 +5,8 @@ const http = require("http");
 const fs = require("fs");
 const ent = require("ent");
 
-const hostname = "0.0.0.0";
-const port = "80";
+const hostname = "127.0.0.1";
+const port = "1337";
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
